36795d19bdb3cc2c3d76778103f2c3a8f583e609
Fernando Freitas <fernandofreitas571@gmail.com>
2025-08-13 20:52:51 -0300

    alterações

--- src/main/java/fernando/com/application/Main.java
+++ src/main/java/fernando/com/application/Main.java
@@ -12,6 +12,5 @@ public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "books");
         System.out.println(obj);
-        System.out.println();
     }
 }
