cdd80ed4fea7685a6e4c77e05da3be9f4e23baa2
Fernando Freitas <fernandofreitas571@gmail.com>
2025-08-13 20:41:31 -0300

    Primeiro commit

--- src/main/java/fernando/com/application/Main.java
+++ src/main/java/fernando/com/application/Main.java
@@ -2,6 +2,7 @@ package fernando.com.application;
 
 import fernando.com.db.DB;
 import fernando.com.db.DbIntegrityException;
+import fernando.com.model.entities.Department;
 
 import java.sql.*;
 import java.text.ParseException;
@@ -9,6 +10,7 @@ import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) {
-
+        Department obj = new Department(1, "books");
+        System.out.println(obj);
     }
 }
