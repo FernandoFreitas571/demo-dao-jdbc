b3807ddea543e0a1cfb455437a131218996aedc7
Fernando Freitas <fernandofreitas571@gmail.com>
2025-08-14 13:19:37 -0300

    DepartmentDao implementation and test

--- src/main/java/fernando/com/model/dao/DaoFactory.java
+++ src/main/java/fernando/com/model/dao/DaoFactory.java
@@ -1,10 +1,14 @@
 package fernando.com.model.dao;
 
 import fernando.com.db.DB;
+import fernando.com.model.dao.impl.DepartmentDaoJDBC;
 import fernando.com.model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
     public static SellerDao createSellerDao(){
         return new SellerDaoJDBC(DB.getConnection());
+    }
+    public static DepartmentDao createDepartmentDao(){
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
