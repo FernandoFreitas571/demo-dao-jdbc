0759bccf5c556055481694be4bd236dbb50c4a75
Fernando Freitas <fernandofreitas571@gmail.com>
2025-08-13 21:02:28 -0300

    Seller entities class

--- src/main/java/fernando/com/application/Main.java
+++ src/main/java/fernando/com/application/Main.java
@@ -1,16 +1,15 @@
 package fernando.com.application;
 
-import fernando.com.db.DB;
-import fernando.com.db.DbIntegrityException;
 import fernando.com.model.entities.Department;
+import fernando.com.model.entities.Seller;
 
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 }
