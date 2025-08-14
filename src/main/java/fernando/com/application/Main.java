18b2e64e2d0074552b9643b7efb0054c625725b2
Fernando Freitas <fernandofreitas571@gmail.com>
2025-08-13 22:10:33 -0300

    findById implementation

--- src/main/java/fernando/com/application/Main.java
+++ src/main/java/fernando/com/application/Main.java
@@ -2,16 +2,13 @@ package fernando.com.application;
 
 import fernando.com.model.dao.DaoFactory;
 import fernando.com.model.dao.SellerDao;
-import fernando.com.model.entities.Department;
 import fernando.com.model.entities.Seller;
-
-import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Department obj = new Department(1, "books");
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
--- src/main/java/fernando/com/model/dao/DaoFactory.java
+++ src/main/java/fernando/com/model/dao/DaoFactory.java
@@ -1,9 +1,10 @@
 package fernando.com.model.dao;
 
+import fernando.com.db.DB;
 import fernando.com.model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
     public static SellerDao createSellerDao(){
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
